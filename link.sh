919a53f538dc6e78027d8fb301ea23f6d6ee0e91
Sergiy Korytskyy <skorytsk@akamai.com>
2023-07-27 22:22:22 +0200

    restructure yabai and skhd and add it them link.sh

--- link.sh
+++ link.sh
@@ -1,3 +1,6 @@
 dir=$(pwd)
 ln -s $dir/.tmux.conf ~/.tmux.conf
 ln -s $dir/.emacs ~/.emacs
+mkdir -p ~/.config/yabai ~/.config/skhd
+ln -s $dir/.config/yabai/yabairc ~/.config/yabai/yabairc
+ln -s $dir/.config/skhd/skhdrc ~/.config/skhd/skhdrc
