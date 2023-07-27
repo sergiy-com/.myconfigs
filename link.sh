225908225b38e929ef87710ddb9791483536d1c4
Sergiy Korytskyy <skorytsk@akamai.com>
2023-07-27 22:27:44 +0200

    relink symbolik link if there is already existent one

--- link.sh
+++ link.sh
@@ -1,6 +1,6 @@
 dir=$(pwd)
-ln -s $dir/.tmux.conf ~/.tmux.conf
-ln -s $dir/.emacs ~/.emacs
+ln -sf $dir/.tmux.conf ~/.tmux.conf
+ln -sf $dir/.emacs ~/.emacs
 mkdir -p ~/.config/yabai ~/.config/skhd
-ln -s $dir/.config/yabai/yabairc ~/.config/yabai/yabairc
-ln -s $dir/.config/skhd/skhdrc ~/.config/skhd/skhdrc
+ln -sf $dir/.config/yabai/yabairc ~/.config/yabai/yabairc
+ln -sf $dir/.config/skhd/skhdrc ~/.config/skhd/skhdrc
